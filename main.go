50a642de7c508d5c73896f5b83433ab372207298
Josh Martin <contact@cjoshmartin.com>
2021-06-24 00:53:00 -0500

    wip first try at webserver

--- main.go
+++ main.go
@@ -2,154 +2,110 @@ package main
 
 import (
 	"context"
-	"log"
+	"encoding/json"
+	"fmt"
+	"net/http"
 	"os"
-
 	"time"
 
+	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 var client *mongo.Client
 
-type Experience struct {
-	ID               primitive.ObjectID   `json:"_id, omitempty" bson:"_id, omitempty"`
-	Name             string               `json:"name, omitempty" bson:"name, omitempty"`
-	Description      string               `json:"description, omitempty" bson:"description, omitempty"[`
-	Attendees        []primitive.ObjectID `json:"attendees, omitempty" bson:"attendees, omitempty"`
-	DateOfExperience primitive.DateTime   `json:"dateofexperience, omitempty" bson:"dateofexperience, omitempty"`
-	Price            float32              `json:"price, omitempty bson:"price, omitempty"`
-}
-type User struct {
-	ID          primitive.ObjectID   `json:"_id, omitempty" bson:"_id, omitempty"`
-	Name        string               `json:"name, omitempty" bson:"name, omitempty"`
-	Email       string               `json:"email, omitempty" bson:"email, omitempty"`
-	Experiences []primitive.ObjectID `json:"experiences, omitempty" bson:"experiences, omitempty"`
-}
-type Chef struct {
-	ID          primitive.ObjectID   `json:"_id, omitempty" bson:"_id, omitempty"`
-	Name        string               `json:"name, omitempty" bson:"name, omitempty"`
-	Experiences []primitive.ObjectID `json:"experiences, omitempty" bson:"experiences, omitempty"`
+type Person struct {
+	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	FirstName string             `json:"firstname,omitempty" bson:"firstname,omitempty"`
+	LastName  string             `json:"lastname,omitempty" bson:"lastname,omitempty"`
 }
 
-type Order struct {
-	ID           primitive.ObjectID `json:"_id, omitempty" bson:"_id, omitempty`
-	ExperienceId primitive.ObjectID `json:"experienceid, omitempty" bson:"experienceid, omitempty"`
-	ChefId       primitive.ObjectID `json:"chefid, omitempty" bson:"chefid, omitempty"`
-	PurchaseTime primitive.DateTime `json:"purchasetime, omitempty" bson:"purchasetime, omitempty"`
-	SubTotal     float32            `json:"subtotal, omitempty" bson:"subtotal, omitempty"`
-	Tip          float32            `json:"tip, omitempty" bson:"tip, omitempty"`
-	Taxes        float32            `json:"taxes, omitempty" bson:"taxes, omitempty"`
-	// Total, calculate it on the fly
+func CreatePersonEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+
+	var person Person
+	_ = json.NewDecoder(request.Body).Decode(&person)
+	collection := client.Database("persondb").Collection("people")
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	result, _ := collection.InsertOne(ctx, person)
+
+	json.NewEncoder(response).Encode(result)
 }
 
+// func GetPersonEndpoint(response http.ResponseWriter, request *http.Request) {
+// 	response.Header().Set("content-type", "application/json")
+
+// 	params := mux.Vars(request)
+// 	id, _ := primitive.ObjectIDFromHex(params["id"])
+
+// 	var person Person
+
+// 	collection := client.Database("persondb").Collection("people")
+// 	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+// 	err := collection.FindOne(ctx, Person{ID: id}).Decode(&person)
+// 	if err != nil {
+// 		response.WriteHeader(http.StatusInternalServerError)
+// 		response.Write([]byte(`{ "message": "` + err.Error() + "}"))
+
+// 		return
+// 	}
+
+// 	json.NewEncoder(response).Encode(person)
+// }
+
+// func GetPeopleEndpoint(response http.ResponseWriter, request *http.Request) {
+// 	response.Header().Set("content-type", "application/json")
+
+// 	var people []Person
+
+// 	collection := client.Database("persondb").Collection("people")
+// 	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+// 	cursor, err := collection.Find(ctx, bson.M{})
+// 	if err != nil {
+// 		response.WriteHeader(http.StatusInternalServerError)
+// 		response.Write([]byte(`{ "message": "` + err.Error() + "}"))
+
+// 		return
+// 	}
+
+// 	defer cursor.Close(ctx)
+// 	for cursor.Next(ctx) {
+// 		var person Person
+// 		cursor.Decode(&person)
+// 		people = append(people, person)
+// 	}
+// 	if err := cursor.Err(); err != nil {
+// 		response.WriteHeader(http.StatusInternalServerError)
+// 		response.Write([]byte(`{ "message": "` + err.Error() + "}"))
+
+// 		return
+// 	}
+
+// 	json.NewEncoder(response).Encode(people)
+// }
 func main() {
-
-	// = "mongodb://0.0.0.0"
-	// = "mongodb://db"
+	fmt.Println("Starting the application...")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 
 	databaseName, exists := os.LookupEnv("DATABASE_CONTAINER_NAME")
+
 	if !exists {
 		databaseName = "0.0.0.0"
 	}
 	mongodbURI := "mongodb://" + databaseName
 
-	client, err := mongo.NewClient(options.Client().ApplyURI(mongodbURI))
+	clientOptions := options.Client().ApplyURI(mongodbURI)
 
-	if err != nil {
-		log.Fatal(err)
-	}
+	client, _ = mongo.Connect(ctx, clientOptions)
+	router := mux.NewRouter()
 
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	router.HandleFunc("/person", CreatePersonEndpoint).Methods("POST")
+	// router.HandleFunc("/people", GetPeopleEndpoint).Methods("GET")
+	// router.HandleFunc("/person/{id}", GetPersonEndpoint).Methods("GET")
 
-	err = client.Connect(ctx)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer client.Disconnect(ctx)
-
-	err = client.Ping(ctx, readpref.Primary())
-	if err != nil {
-		log.Fatal(err)
-	}
+	fmt.Print("Listening on port: 12345")
+	http.ListenAndServe(":12345", router)
 
 }
-
-// inspecting databases
-// "go.mongodb.org/mongo-driver/bson"
-
-// databases, err := client.ListDatabaseNames(ctx, bson.M{})
-// if err != nil {
-// 	log.Fatal(err)
-// }
-// fmt.Println(databases)
-
-// inserting records into database
-
-// quickstartDatabase := client.Database("quickstart")
-// podcastsCollection := quickstartDatabase.Collection("podcasts")
-// episodesCollection := quickstartDatabase.Collection("episodes")
-
-// podcastResult, err := podcastsCollection.InsertOne(ctx, bson.D{
-// 	{Key: "title", Value: "The Polyglot Developer Podcast"},
-// 	{Key: "author", Value: "Nic Raboy"},
-// })
-
-// episodeResult, err := episodesCollection.InsertMany(ctx, []interface{}{
-// 	bson.D{
-// 		{"podcast", podcastResult.InsertedID},
-// 		{"title", "GraphQL for API Development"},
-// 		{"description", "Learn about GraphQL from the co-creator of GraphQL, Lee Byron."},
-// 		{"duration", 25},
-// 	},
-// 	bson.D{
-// 		{"podcast", podcastResult.InsertedID},
-// 		{"title", "Progressive Web Application Development"},
-// 		{"description", "Learn about PWA development with Tara Manicsic."},
-// 		{"duration", 32},
-// 	},
-// })
-
-// if err != nil {
-// 	log.Fatal(err)
-// }
-// fmt.Printf("Inserted %v documents into episode collection!\n", len(episodeResult.InsertedIDs))
-
-// Looking for records
-// quickstartDatabase := client.Database("quickstart")
-// podcastsCollection := quickstartDatabase.Collection("podcasts")
-// episodesCollection := quickstartDatabase.Collection("episodes")
-
-// cursor, err := podcastsCollection.Find(ctx, bson.M{})
-// if err != nil {
-// 	log.Fatal(err)
-// }
-// defer cursor.Close(ctx)
-
-// for cursor.Next(ctx) {
-// 	var podcast bson.M
-// 	if err = cursor.Decode(&podcast); err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	fmt.Println(podcast)
-// }
-
-// cursor, err = episodesCollection.Find(ctx, bson.M{})
-
-// if err != nil {
-// 	log.Fatal(err)
-// }
-// defer cursor.Close(ctx)
-
-// for cursor.Next(ctx) {
-// 	var episode bson.M
-// 	if err = cursor.Decode(&episode); err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	fmt.Println(episode)
-// }
